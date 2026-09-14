ca5fd1788c84809435f5231194f8febe6ce7ee69
g5instr <g5instr@gmail.com>
2026-09-14 13:38:53 -0300

    Home.py

--- Home.py
+++ Home.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
-st.set_page_config(page_title="Painel de Ferramentas", layout="wide")
+st.set_page_config(page_title="Conversor de arquivos CSV WS", layout="wide")
 
-st.title("🏠 Painel de Ferramentas")
+st.title("🏠 Conversor de arquivos CSV WS")
 st.write("Selecione uma das opções abaixo:")
 
-st.page_link("pages/leituraCompacted_vw.py", label="📊 Gráficos de arquivo csv (Corda Vibrante)", icon="📊")
-st.page_link("pages/lerCompacted_tm.py", label="🧩 Gráficos de arquivo csv (Tiltímetros)", icon="🧩")
+st.page_link("pages/leituraCompacted_vw.py", label="Gráficos de arquivo csv (Corda Vibrante - 4 horas)")
+st.page_link("pages/lerCompacted_tm.py", label="Gráficos de arquivo csv (Tiltímetros - 2 minutos)")
